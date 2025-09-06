7ed7f22a4620b54b4d5710e29d9f63f05523f000
SIRIUSDBLACK <tzaw3720@gmail.com>
2025-09-06 10:28:20 +0630

    fix : detail route id fix

--- src/routes/job.routes.ts
+++ src/routes/job.routes.ts
@@ -7,7 +7,7 @@ const router = express.Router()
 
 router.post("/create",authenticationMiddleware,authorizationMiddleware(['employer']),createJob as any);
 router.get("/",authenticationMiddleware,getJobs);
-router.get("/job-detail",authenticationMiddleware,GetAJob as any);
+router.get("/job-detail/:id",authenticationMiddleware,GetAJob as any);
 router.get("/myjobs",authenticationMiddleware,authorizationMiddleware(['employer']),getMyJobs as any);
 router.put("/update/:id",authenticationMiddleware,authorizationMiddleware(['employer']),updateExistingJob as any);
 router.delete("/delete/:id",authenticationMiddleware,authorizationMiddleware(['employer']),deleteExistingJob as any);
